1e2d1122a9085bb226cf4755bdfc39af7af3bb1e
first <first@DESKTOP-TFIJVBI>
2025-10-21 03:26:45 +0500

    Возвращение работоспособности сайта

---
+++ Feane/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Feane.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Book()
+        {
+            return View();
+        }
+    }
+}
---
+++ Feane/Controllers/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Feane.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
