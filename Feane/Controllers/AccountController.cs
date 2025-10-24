95f27c693e3a1940f5d9abe6a3f68489f552f831
first <first@DESKTOP-TFIJVBI>
2025-10-25 01:20:40 +0500

    Добавление страницы авторизации

---
+++ Feane/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Feane.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult LogInOut()
+        {
+            return View();
+        }
+    }
+}
