383c60a1df2b09139cc856262566ff809b800324
first <first@DESKTOP-TFIJVBI>
2025-12-09 04:35:05 +0500

    Revert "мусор"
    This reverts commit 19e0a30b2b2428929ecaf12a9c760299bb2ab34f.

--- Feane/Program.cs
+++ Feane/Program.cs
@@ -1,34 +1,11 @@
-using System.Globalization;
-using Microsoft.AspNetCore.Localization;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Localization
-builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-
-builder.Services.AddControllersWithViews()
-    .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix)
-    .AddDataAnnotationsLocalization();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Supported cultures
-var supportedCultures = new[] { new CultureInfo("ru"), new CultureInfo("en") };
-
-var localizationOptions = new RequestLocalizationOptions
-{
-    DefaultRequestCulture = new RequestCulture("ru"),
-    SupportedCultures = supportedCultures,
-    SupportedUICultures = supportedCultures
-};
-
-// Providers: cookie first, then Accept-Language header
-var cookieProvider = new CookieRequestCultureProvider();
-var acceptLangProvider = new AcceptLanguageHeaderRequestCultureProvider();
-localizationOptions.RequestCultureProviders = new IRequestCultureProvider[] { cookieProvider, acceptLangProvider };
-
-app.UseRequestLocalization(localizationOptions);
-
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -37,7 +14,6 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication(); // если есть аутентификация
 app.UseAuthorization();
 
 app.MapControllerRoute(
