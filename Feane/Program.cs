19e0a30b2b2428929ecaf12a9c760299bb2ab34f
first <first@DESKTOP-TFIJVBI>
2025-12-09 04:34:50 +0500

    мусор

--- Feane/Program.cs
+++ Feane/Program.cs
@@ -1,11 +1,34 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// Localization
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Supported cultures
+var supportedCultures = new[] { new CultureInfo("ru"), new CultureInfo("en") };
+
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("ru"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
+
+// Providers: cookie first, then Accept-Language header
+var cookieProvider = new CookieRequestCultureProvider();
+var acceptLangProvider = new AcceptLanguageHeaderRequestCultureProvider();
+localizationOptions.RequestCultureProviders = new IRequestCultureProvider[] { cookieProvider, acceptLangProvider };
+
+app.UseRequestLocalization(localizationOptions);
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -14,6 +37,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // если есть аутентификация
 app.UseAuthorization();
 
 app.MapControllerRoute(
