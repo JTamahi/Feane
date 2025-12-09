0296d81d3b4358112d6cae849f4162cdace6ad31
first <first@DESKTOP-TFIJVBI>
2025-12-09 05:25:06 +0500

    перевел часть проекта

---
+++ Feane/Controllers/CultureController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Feane.Controllers
+{
+    public class CultureController : Controller
+    {
+        [HttpGet]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            if (string.IsNullOrEmpty(culture))
+            {
+                culture = "en";
+            }
+
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new Microsoft.AspNetCore.Localization.RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            if (string.IsNullOrEmpty(returnUrl))
+            {
+                returnUrl = Url.Action("Index", "Home");
+            }
+
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
--- Feane/Program.cs
+++ Feane/Program.cs
@@ -1,9 +1,33 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// Localization
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
 
 var app = builder.Build();
+
+// Configure supported cultures
+var supportedCultures = new[]
+{
+    new CultureInfo("en"),
+    new CultureInfo("ru")
+};
+
+var requestLocalizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
+
+// Use localization
+app.UseRequestLocalization(requestLocalizationOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
